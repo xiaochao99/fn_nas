a98f493909fa8362e507781481618a3c629029fe
xiaochao <xiaochao@hypernas.cn>
2026-01-13 17:08:19 +0800

    修复设置报错问题

--- custom_components/fn_nas/config_flow.py
+++ custom_components/fn_nas/config_flow.py
@@ -189,13 +189,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
-        return OptionsFlowHandler(config_entry)
+        return OptionsFlowHandler()
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
     """处理飞牛NAS的选项流程"""
-    
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
     
     async def async_step_init(self, user_input=None):
         if user_input is not None:
